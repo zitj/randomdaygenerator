5f8fbdbe230c400af889d3257f57426883c964ad
zitj <1eyedea2eyedeas@gmail.com>
2020-08-14 14:39:24 +0200

    finished project

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
 const generateButton = document.querySelector('button');
 const heading = document.querySelector('h1');
+const images = document.querySelector('img');
+const paragraph = document.querySelectorAll('p');
 
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 
 generateButton.addEventListener('click', (min, max) =>{
     min = 1;
     max = 7;
     let random = Math.floor(Math.random() * (max - min)) + min;
-    heading.innerHTML = weekDays[random];    
+    heading.innerHTML = weekDays[random];
+    images.classList.add('active');
+    images.addEventListener('animationend', () => {
+        paragraph[1].classList.add('active');
+      });
+    paragraph[1].addEventListener('animationend', ()=>{
+        heading.classList.add('active');
+    });  
 });
